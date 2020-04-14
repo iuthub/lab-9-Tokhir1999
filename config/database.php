d7800e2540d5271e7d65bfd2fb0f657459188072
Tokhir1999 <45263692+Tokhir1999@users.noreply.github.com>
2020-04-14 15:07:17 +1400

    Finished Assignment

--- config/database.php
+++ config/database.php
@@ -64,6 +64,12 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'modes'  => [
+                 'ONLY_FULL_GROUP_BY',
+                 'STRICT_TRANS_TABLES','NO_ZERO_IN DATE','NO_ZERO_DATE'
+                 'ERROR_FOR_DIVISION_BY_ZERO',
+                 'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
@@ -93,6 +99,7 @@ return [
     */
 
     'migrations' => 'migrations',
+    
 
     /*
     |--------------------------------------------------------------------------
